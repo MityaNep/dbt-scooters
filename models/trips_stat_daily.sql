33e39d7496002fdf176d418d1e23ef8a1101b572
Dmitrii Neplokhov <mityanep@mail.ru>
2026-07-16 00:53:30 +0300

    update and add new models

--- models/trips_stat_daily.sql
+++ models/trips_stat_daily.sql
@@ -1,10 +1,10 @@
 select
-    date(started_at) as date,
+    date,
     count(*) as trips,
-    max(price) / 100 as max_price_rub,
-    avg(distance) / 1000 as avg_distance_km
-from
-    scooters_raw.trips
+    max(price_rub) as max_price_rub,
+    avg(distance_m) / 1000 as avg_distance_km,
+    sum(price_rub)/sum(duration_s)*60 as avg_price_rub_per_min
+from {{ ref("trips_prep") }}
 group by
     1
 order by
