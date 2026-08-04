2410e3fa4cc1c22d11229c975d893b273bdc1dbf
Dmitrii Neplokhov <mityanep@mail.ru>
2026-08-05 00:40:27 +0300

    update

---
+++ macros/create_role.sql
@@ -0,0 +1,8 @@
+{% macro create_role(name) %}
+    {% set query %}
+        create role {{name}} nologin;
+    {% endset %}
+    {% do log("Creating role: " ~ name , info=True) %}
+    {% do run_query(query) %}
+{% endmacro %}
+  
